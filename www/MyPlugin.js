9475522bd9d1c0a1a911d3f713f88f0e824885c3
vishucsuddanti <vishwauddanti@yahoo.in>
2014-11-12 13:33:37 +0530

    first commit

---
+++ www/MyPlugin.js
@@ -0,0 +1,14 @@
+var exec = require('cordova/exec');
+/**
+ * Constructor
+ */
+               function MyPlugin() {}
+               
+               MyPlugin.prototype.sayHello = function(win,fail,options) {
+               exec(win,fail, "MyPlugin", "sayHello",options);
+               }
+               
+               var myPlugin = new MyPlugin();
+               module.exports = myPlugin
+               });
+
